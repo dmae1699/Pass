6ec76dfb7c4da6b942c096393eaf72b3ee24ab49
dmae1699 <dmae1699@gmail.com>
2026-08-10 19:33:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-#Write a program to satisfy the following conditions of the given range: If the number is divisible by 20, 
-# it provides an output "twist." If the number is divisible by 15, it will pass (no output) If the number 
-# is divisible by 5, it will give an output “fizz.” If the number is divisible by 3, it will give an output
-#  "buzz." Otherwise, it will give the output of that number.
-
 num = int(input("Enter number: "))
 
 if num%20 == 0:
